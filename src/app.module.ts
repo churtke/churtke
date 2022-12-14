6da714b670aa7a0293ca6c8f503e95bb1aa8942e
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2022-12-14 13:59:38 +0330

    ADD: config file parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { validationSchema } from './common/config/validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
