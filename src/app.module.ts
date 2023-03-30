706f2e30dc220def4eb9e76da363fbbbf49f84da
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2023-03-30 23:34:03 +0330

    ADD: config loader from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './common/config/validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  const PORT = app.get(ConfigService).get<number>('PORT');
+
+  await app.listen(PORT, () => {
+    Logger.log(`Listening at http://localhost:${PORT}`);
+  });
 }
 bootstrap();
