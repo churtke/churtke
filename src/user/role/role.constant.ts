cd26771e27a1c8704206813b8fc996d483be9260
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2023-07-30 11:40:28 +0330

    ADD: product access

--- src/user/role/role.constant.ts
+++ src/user/role/role.constant.ts
@@ -4,6 +4,10 @@ export enum Action {
 
   FILE_VIEW = 'file_view',
   FILE_ADD = 'file_add',
+
+  PRODUCT_VIEW = 'product_view',
+  PRODUCT_ADD = 'product_add',
+  PRODUCT_EDIT = 'product_edit',
 }
 
 export const Customer: string[] = [Action.PROFILE_VIEW, Action.PROFILE_EDIT];
@@ -12,6 +16,9 @@ export const Manager: string[] = [
   ...Customer,
   Action.FILE_VIEW,
   Action.FILE_ADD,
+  Action.PRODUCT_VIEW,
+  Action.PRODUCT_ADD,
+  Action.PRODUCT_EDIT,
 ];
 
 export const Admin: string[] = [...Object.values(Action)];
